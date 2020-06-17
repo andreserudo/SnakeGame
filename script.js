2b0b55e155d8fa2df52ddc50ed454a9fd495e96d
Andre Serudo <andreserudo@gmail.com>
2020-06-16 22:05:13 -0400

    drawing food and snake grow

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-let direction = "right";
+let direction = "up";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 snake[0] = {
   x: 8 * box,
   y: 8 * box
 };
+
 
 function createBG(){
   context.fillStyle = "lightgreen";
@@ -21,13 +26,50 @@ function createSnake(){
   }
 }
 
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+  console.log(event.keyCode);
+  if(event.keyCode === 37 && direction != "right")
+    direction = "left";
+
+  if(event.keyCode === 39 && direction != "left")
+    direction = "right";
+
+  if(event.keyCode === 40 && direction != "up")
+    direction = "up";
+
+  if(event.keyCode === 38 && direction != "down")
+    direction = "down";
+
+}
+
 function startGame(){
+  if(snake[0].x > 15 * box && direction === "right")
+    snake[0].x = 0;
+
+  if(snake[0].x < 0 && direction === "left")
+    snake[0].x = 16 * box;    
+
+  if(snake[0].y > 15 * box && direction === "up"){
+    snake[0].y = 0;
+  }
+
+  if(snake[0].y < 0 && direction === "down")
+    snake[0].y = 16 * box;    
+
   createBG();
   createSnake();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
-
+  //console.log(snakeY);
   if(direction == "right") 
     snakeX += box;
   if(direction == "left") 
@@ -37,7 +79,14 @@ function startGame(){
   if(direction == "down") 
     snakeY -= box;
 
-  snake.pop();
+  if(snakeX !== food.x || snakeY !== food.y){    
+    snake.pop()
+  }else{
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;    
+  }
+
+  //snake.pop();
 
   let newHead = {
     x: snakeX,
